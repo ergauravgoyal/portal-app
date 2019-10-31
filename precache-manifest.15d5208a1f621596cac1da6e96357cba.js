1f6d7526e1b585a24ec4ec2fbe3182b34f550019
Gaurav Goyal <gaurav_goyal-ggn@external.mckinsey.com>
2019-10-31 19:23:18 +0530

    Updates

---
+++ precache-manifest.15d5208a1f621596cac1da6e96357cba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfa1f05a90bc7372e6362034cd51063",
+    "url": "/portal-app-demo/index.html"
+  },
+  {
+    "revision": "bd8ac94999764275e1cc",
+    "url": "/portal-app-demo/static/css/main.7c297a5a.chunk.css"
+  },
+  {
+    "revision": "3e1492bcca47815c31f4",
+    "url": "/portal-app-demo/static/js/2.62debea1.chunk.js"
+  },
+  {
+    "revision": "bd8ac94999764275e1cc",
+    "url": "/portal-app-demo/static/js/main.49199e81.chunk.js"
+  },
+  {
+    "revision": "ab14262f9faaccf5c28a",
+    "url": "/portal-app-demo/static/js/runtime-main.a4d86d9f.js"
+  }
+]);
